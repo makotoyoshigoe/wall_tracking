99b7cc2188b3313f370c1b6b543938f323704144
makotoyoshigoe <myoshigo0127@gmail.com>
2024-02-16 19:02:11 +0900

    Change method of function

--- src/ScanData.cpp
+++ src/ScanData.cpp
@@ -61,19 +61,24 @@ float ScanData::leftWallCheck(float start_deg, float end_deg)
     return per;
 }
 
-float ScanData::openPlaceCheck(float start_deg, float end_deg, float threshold)
+void ScanData::openPlaceCheck(float start_deg, float end_deg, float threshold, float &per, float &mean_l)
 {
     // RCLCPP_INFO(rclcpp::get_logger("ScanData"), "start: %f, end: %f", start_deg, end_deg);
     float start_index = deg2index(start_deg);
     float end_index = deg2index(end_deg);
-    int sum = 0, sum_i = 0;
+    int sum = 0, sum_i = 0, sum_n = 0.;
+    float sum_l = 0.;
     for(int i=start_index; i<=end_index; ++i){
         float range = tmp_scan_msg_->ranges[i];
-        sum += (range < range_min_ || range > threshold || range == INFINITY);
+        sum += (range < range_min_ || range >= threshold || range == INFINITY);
+        if(range >= threshold){
+            sum_l += range;
+            ++sum_n;
+        }
         ++sum_i;
     }
-    float per = static_cast<float>(sum) / static_cast<float>(sum_i);
-    return per;
+    per = static_cast<float>(sum) / static_cast<float>(sum_i);
+    mean_l = sum_l / static_cast<float>(sum_n);
 }
 
 bool ScanData::conflictCheck(float deg, float threshold)
